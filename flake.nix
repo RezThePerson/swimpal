7fc2a7407416f92a0e46e4a688d4e1e20bcbd211
RezThePerson <rhythmupa@gmail.com>
2026-03-06 14:47:53 +0530

    first commit

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "My Burnt Toast's NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          inherit system;
+
+          specialArgs = {
+            inherit inputs;
+            modules = [
+
+            ];
+          };
+
+          modules = [
+            ./host/hardware-configuration.nix
+            ./core
+            ./modules
+          ];
+        };
+      };
+    };
+}
